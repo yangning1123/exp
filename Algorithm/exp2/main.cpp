da9a24345a455f5e7a2161d42a0c9cebc81e2dd7
yangning <yangning1123@gmail.com>
2013-12-30 19:06:41 +0800

    引进了try、catch功能

--- Algorithm/exp2/main.cpp
+++ Algorithm/exp2/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include "Dynamic.h"
-using std::cout; using std::endl;
+using std::cout; using std::cerr; using std::endl;
 using std::ifstream;
+using std::runtime_error;
 using namespace exp2;
 
 int main(int argc, char *argv[])
@@ -10,9 +11,14 @@ int main(int argc, char *argv[])
     Dynamic<int> dy(10);
     ifstream ifs("data.txt");
 
+    try {
     dy.read(ifs);
     dy.Dynamic_fun();
     dy.print(cout);
+    } catch (runtime_error &e)
+    {
+	cerr << e.what();
+    }
     cout << endl;
     
     return 0;
